187eca2f94ae01ee68540be046228e328d34a22d
victorialokteva <vilokteva@yandex.ru>
2024-01-10 15:38:21 -0500

    code refactoring

--- ML/preprocessing.py
+++ ML/preprocessing.py
@@ -1,87 +1,119 @@
 import pandas as pd
-from typing import Tuple
 
 
-def preprocessing(interview_x_path: str, interview_y_path: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    """"""
-    interview_x = pd.read_csv(interview_x_path)
-    interview_y = pd.read_csv(interview_y_path)
-    # уберем дупликаты
-    interview_x = interview_x.drop_duplicates()
-    interview_y = interview_y.drop_duplicates()
-    # заполним пропуски для категориальных фич новым значением
-    for col in ['osName', 'model', 'hardware']:
-        interview_x.loc[interview_x[col].isna(), col] = 'unknown'
+class DataPreprocessor(object):
 
-    # конвертируем строки в даты
-    interview_x['reg_time'] = pd.to_datetime(interview_x['reg_time'], format='%Y-%m-%d %H:%M:%S')
+    def __init__(self):
+        self.train_path = "data/interview.X.csv"
+        self.test_path = "data/interview.y.csv"
 
-    # из site_id можно извлечь много информации
-    # уберем www, www1
-    interview_x['site_id'] = interview_x['site_id'].map(
-        lambda x: x.replace("www1.", "").replace("www.", ""))
-    # Добавим фичу домен
-    interview_x['domain'] = interview_x['site_id'].map(lambda x: x.split('.')[-1])
-    interview_x = extract_catgories(interview_x)
+    def preprocessing(self) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        """"""
+        train = pd.read_csv(self.train_path)
+        test = pd.read_csv(self.test_path)
 
-    # значения dma, которые встречаются менее 5 раз, объединим в одну категорию -1 ("другое")
-    rare_dma = [dma for dma, count in interview_x['mm_dma'].value_counts().items() if count < 5]
-    interview_x.loc[interview_x['mm_dma'].isin(rare_dma), 'mm_dma'] = -1
+        train = train.drop_duplicates()
+        test = test.drop_duplicates()
 
-    # Аналогично поступим с site_id, model
-    rare_site = [site for site, count in interview_x['site_id'].value_counts().items() if count < 5]
-    interview_x.loc[interview_x['site_id'].isin(rare_site), 'site_id'] = "other"
+        # fill nans for categorical features
+        for col in ['osName', 'model', 'hardware']:
+            train.loc[train[col].isna(), col] = 'unknown'
 
-    rare_model = [model for model, count in interview_x['model'].value_counts().items() if count < 5]
-    interview_x.loc[interview_x['model'].isin(rare_model), 'model'] = "other"
+        train = self._website_name_preprocessing(train)
 
-    interview = interview_x.merge(interview_y, how='inner', on='uid')
+        # значения dma, которые встречаются менее 5 раз, объединим в одну категорию -1 ("другое")
+        rare_dma = [dma for dma, count in train['mm_dma'].value_counts().items() if count < 5]
+        train.loc[train['mm_dma'].isin(rare_dma), 'mm_dma'] = -1
 
-    return interview_x, interview_y, interview
+        # Аналогично поступим с site_id, model
+        rare_site = [site for site, count in train['site_id'].value_counts().items() if count < 5]
+        train.loc[train['site_id'].isin(rare_site), 'site_id'] = "other"
 
+        rare_model = [model for model, count in train['model'].value_counts().items() if count < 5]
+        train.loc[train['model'].isin(rare_model), 'model'] = "other"
 
-def extract_catgories(interview_x: pd.DataFrame) -> pd.DataFrame:
-    """Добавим категории (спорт/здоровье и т.д.), к которым относится сайт, по ключевым словам из названия"""
-    interview_x['sport_cat'] = (interview_x['site_id'].str.contains('sport')
-                                | interview_x['site_id'].str.contains('baseball')
-                                | interview_x['site_id'].str.contains('football')
-                                | interview_x['site_id'].str.contains('soccer')
-                                | interview_x['site_id'].str.contains('basketball')
-                                | interview_x['site_id'].str.contains('hockey')
-                                | interview_x['site_id'].str.contains('moneyball')
-                                | interview_x['site_id'].str.contains('rotowire')
-                                | interview_x['site_id'].str.contains('mmafighting')
-                                | interview_x['site_id'].str.contains('mmamania')
-                                | interview_x['site_id'].str.contains('nbaanalysis')
-                                | interview_x['site_id'].str.contains('nba-trade-rumors')
-                                | interview_x['site_id'].str.contains('nfltraderumors')
-                                | interview_x['site_id'].str.contains('nfldraftdiamonds')
-                                | interview_x['site_id'].str.contains('detroitbadboys.com')  # ?
-                                | interview_x['site_id'].str.contains('volleyball'))
+        data = train.merge(test, how='inner', on='uid')
+        data = self._date_preprocessing(data)
+        data = self._targets(data)
 
-    interview_x['travel_cat'] = (interview_x['site_id'].str.contains('travel')
-                                 | interview_x['site_id'].str.contains('tourist'))
-    interview_x['weather_cat'] = (interview_x['site_id'].str.contains('weather'))
-    interview_x['game_cat'] = (interview_x['site_id'].str.contains('game'))
-    interview_x['music_cat'] = (interview_x['site_id'].str.contains('music')
-                                | interview_x['site_id'].str.contains('hip-hop')
-                                | interview_x['site_id'].str.contains('song')  # ?
-                                | interview_x['site_id'].str.contains('guitar'))
+        data.to_csv("/Users/victorialokteva/Downloads/interview.csv")
 
-    interview_x['cook_cat'] = (interview_x['site_id'].str.contains('cook')
-                               | interview_x['site_id'].str.contains('diet')
-                               | interview_x['site_id'].str.contains('fastfood')
-                               | interview_x['site_id'].str.contains('cupcake')  # javacupcake.com
-                               | interview_x['site_id'].str.contains('grandbaby-cakes')
-                               | interview_x['site_id'].str.contains('browneyedbaker')  # baker?
-                               | interview_x['site_id'].str.contains('baking')
-                               | interview_x['site_id'].str.contains('lovebakesgoodcakes')
-                               | interview_x['site_id'].str.contains('bakery'))
+        return train, test, data
 
-    interview_x['health_cat'] = (interview_x['site_id'].str.contains('health')
-                                 | interview_x['site_id'].str.contains('medical'))
+    @staticmethod
+    def _date_preprocessing(data: pd.DataFrame) -> pd.DataFrame:
+        """"""
+        # convert strings into dates
+        data['reg_time'] = pd.to_datetime(data['reg_time'], format='%Y-%m-%d %H:%M:%S')
 
-    interview_x['zodiac_cat'] = (interview_x['site_id'].str.contains('astrology')
-                                 | interview_x['site_id'].str.contains('zodiac'))
+        data['month'] = data['reg_time'].dt.month
+        data['hour'] = data['reg_time'].dt.hour
+        data['day'] = pd.to_datetime(data['reg_time']).dt.day_name()
+        return data
 
-    return interview_x
+    @staticmethod
+    def _targets(data: pd.DataFrame) -> pd.DataFrame:
+        """"""
+        # ???
+        # событие fclick - первый клик (используется для расчета CTR).
+        data['click'] = (data['tag'] == 'fclick')
+        return data
+
+    @staticmethod
+    def _website_name_preprocessing(train: pd.DataFrame) -> pd.DataFrame:
+        """Extract information from website names.
+        Add categories such as sports and health based on keywords."""
+
+        # remove www, www1
+        train['site_id'] = train['site_id'].map(lambda x: x.replace("www1.", "").replace("www.", ""))
+
+        # add a feature with domain-related information.
+        train['domain'] = train['site_id'].map(lambda x: x.split('.')[-1])
+
+        #
+        train['is_sport_site'] = (train['site_id'].str.contains('sport')
+                                  | train['site_id'].str.contains('baseball')
+                                  | train['site_id'].str.contains('football')
+                                  | train['site_id'].str.contains('soccer')
+                                  | train['site_id'].str.contains('basketball')
+                                  | train['site_id'].str.contains('hockey')
+                                  | train['site_id'].str.contains('moneyball')
+                                  | train['site_id'].str.contains('rotowire')
+                                  | train['site_id'].str.contains('mmafighting')
+                                  | train['site_id'].str.contains('mmamania')
+                                  | train['site_id'].str.contains('nbaanalysis')
+                                  | train['site_id'].str.contains('nba-trade-rumors')
+                                  | train['site_id'].str.contains('nfltraderumors')
+                                  | train['site_id'].str.contains('nfldraftdiamonds')
+                                  | train['site_id'].str.contains('detroitbadboys.com')  # ?
+                                  | train['site_id'].str.contains('volleyball'))
+
+        train['is_travel_site'] = (train['site_id'].str.contains('travel')
+                                   | train['site_id'].str.contains('tourist'))
+
+        train['is_weather_site'] = (train['site_id'].str.contains('weather'))
+
+        train['is_game_site'] = (train['site_id'].str.contains('game'))
+
+        train['is_music_site'] = (train['site_id'].str.contains('music')
+                                  | train['site_id'].str.contains('hip-hop')
+                                  | train['site_id'].str.contains('song')  # ?
+                                  | train['site_id'].str.contains('guitar'))
+
+        train['is_cook_site'] = (train['site_id'].str.contains('cook')
+                                 | train['site_id'].str.contains('diet')
+                                 | train['site_id'].str.contains('fastfood')
+                                 | train['site_id'].str.contains('cupcake')  # javacupcake.com
+                                 | train['site_id'].str.contains('grandbaby-cakes')
+                                 | train['site_id'].str.contains('browneyedbaker')  # baker?
+                                 | train['site_id'].str.contains('baking')
+                                 | train['site_id'].str.contains('lovebakesgoodcakes')
+                                 | train['site_id'].str.contains('bakery'))
+
+        train['is_health_site'] = (train['site_id'].str.contains('health')
+                                   | train['site_id'].str.contains('medical'))
+
+        train['is_zodiac_site'] = (train['site_id'].str.contains('astrology')
+                                   | train['site_id'].str.contains('zodiac'))
+
+        return train
